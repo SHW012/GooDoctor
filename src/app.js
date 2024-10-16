32747149405ed0ff126e82e1d9806cffa24dc526
SeonHyeonWook <seonhyunuk@gmail.com>
2024-10-16 17:47:13 +0900

    피드백 페이지 추가 후 제작 중

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import LandingPage from "./LandingPage";
 import SignUp from "./Signup";
 import Login from "./Login";
 import SpecPage from "./components/SpecPage";
+import FeedbackPage from "./feedback/FeedbackPage"; // FeedbackPage 경로 수정
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/spec" element={<SpecPage />} />
+          <Route path="/feedback" element={<FeedbackPage />} />{" "}
+          {/* 피드백 페이지 라우트 */}
         </Routes>
       </div>
     </Router>
---
+++ src/feedback/FeedbackData.js
@@ -0,0 +1,27 @@
+// 파일 이름: FeedbackData.js
+
+// 각 항목별 피드백 데이터
+export const specData = {
+  학력: [{ title: "학력", content: "한국항공대학교 경영학부 학점 4.0" }],
+  어학: [{ title: "어학 1", content: "TOEIC 990" }],
+  자격증: [
+    { title: "자격증 1", content: "GTQ 1급" },
+    { title: "자격증 2", content: "6시그마 GB" },
+  ],
+  수상: [
+    { title: "수상 1", content: "한국항공대학교 보잉대회 [우수상]" },
+    { title: "수상 2", content: "한국학회 마케팅 공모전 [우수상]" },
+  ],
+  교내외활동: [
+    { title: "프로젝트 1", content: "#대외 네이처리퍼블릭 서포터즈 6개월" },
+    { title: "프로젝트 2", content: "#교내 비즈니스 해커톤" },
+  ],
+  인턴십경력: [
+    { title: "인턴 1", content: "한진 인사팀 2개월" },
+    { title: "경력 1", content: "AA기업 계약직 마케팅부서 3개월" },
+  ],
+  강점약점: [
+    { title: "강점", content: "#기획력 #분석능력" },
+    { title: "약점", content: "#우유부단 #급한성격" },
+  ],
+};
